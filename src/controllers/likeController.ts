cb26c8f1293ad4b6585716ecb150e1a376436cde
kayigmb <146448328+kayigmb@users.noreply.github.com>
2024-02-19 12:14:25 +0200

    ft-likes

--- src/controllers/likeController.ts
+++ src/controllers/likeController.ts
@@ -28,11 +28,11 @@ const like = async (req: Request, res: Response) => {
         if (userLikedIndex === -1) {
             blog.likes.push({ user: userIdObject });
             await blog.save();
-            res.status(200).send('Blog post was liked ');
+            res.status(200).send('Blog post liked ');
         } else {
             blog.likes.splice(userLikedIndex, 1);
             await blog.save();
-            res.status(200).send('Blog post was unliked');
+            res.status(200).send('Blog post unliked');
         }
     } catch (error) {
         console.error("Internal error:", error);

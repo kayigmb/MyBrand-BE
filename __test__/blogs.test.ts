856644c5c53710d30bedcab29a37f2b0b232c68a
kayigmb <146448328+kayigmb@users.noreply.github.com>
2024-02-27 23:13:24 +0200

    fix circle ci

--- __test__/blogs.test.ts
+++ __test__/blogs.test.ts
@@ -8,11 +8,11 @@ import { title } from 'process';
 
 dotenv.config();
 
-const ENV_db_URL ="mongodb+srv://kayigm1:kayigm132@post.kwblqh9.mongodb.net/?retryWrites=true&w=majority ";
+
 
   
 beforeAll(async () => {
-    await mongoose.connect(ENV_db_URL);
+    await mongoose.connect("mongodb+srv://kayigm1:kayigm132@post.kwblqh9.mongodb.net/?retryWrites=true&w=majority");
 }, 50000);
 
 afterAll(async () => {

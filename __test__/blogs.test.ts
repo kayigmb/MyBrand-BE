66258c70eb92e672fe318ebd3721758355733821
kayigmb <146448328+kayigmb@users.noreply.github.com>
2024-03-03 19:10:39 +0200

    .env

--- __test__/blogs.test.ts
+++ __test__/blogs.test.ts
@@ -9,10 +9,10 @@ import { title } from 'process';
 dotenv.config();
 
 
-// const db = process.env.DATABASE_URL
+const db = process.env.DATABASE_URL
   
 beforeAll(async () => {
-    await mongoose.connect("mongodb+srv://kayigm1:kayigm132@post.kwblqh9.mongodb.net/?retryWrites=true&w=majority");
+    await mongoose.connect(db as string);
 }, 50000);
 
 afterAll(async () => {
